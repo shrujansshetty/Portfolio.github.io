3524dfc1999dfb32d0c73471a8329edfc6bd84a0
Shrujan S Shetty <142001419+shrujansshetty@users.noreply.github.com>
2025-11-13 18:11:19 +0530

    Update project image format from PNG to JPG

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -12,7 +12,7 @@ import { ArrowRight } from "lucide-react";
 const arrproject = [
   {
     projectname: "MetLife",
-    image: "./projects/metlife.png",
+    image: "./projects/metlife.jpg",
     discription:
       "A doctor appointment booking and managing platform, built using spring boot.",
     link: "https://github.com/shrujansshetty/MetLife-SpringBoot",
